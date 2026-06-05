b42e4b61275e56f4f3229eefec9384ad6053a476
Tahsina Tasneem <tahsinatasneem08@gmail.com>
2026-06-06 01:03:46 +0600

    glass navbar upgrade

--- script.js
+++ script.js
@@ -14,6 +14,23 @@ window.addEventListener("load", () => {
 
 });
 
+// GLASS NAVBAR SCROLL EFFECT
+
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+
+  if(window.scrollY > 50){
+
+    navbar.classList.add("scrolled");
+
+  } else {
+
+    navbar.classList.remove("scrolled");
+  }
+
+});
+
 
 // Project Details Modal
 
